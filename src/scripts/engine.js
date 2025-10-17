8758b4a9362ba047f72a518f75bb7277aa1e456f
Naiaque <nfreiredecarvalho@gmail.com>
2025-10-16 21:24:58 -0300

    Feat: Adiciona efeitos sonoros e corrige a lógica do jogo
    - Adiciona sons para virar as cartas, encontrar pares e completar o jogo.
    - Corrige bug que permitia que um clique duplo na mesma carta.
    - Impede que cartas já encontradas possam ser selecionadas novamente.
    - Altera o cursor 'pointer' para ser exibido apenas no verso da carta.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -16,35 +16,51 @@ const emojis = [
     "🐒",
     "🐒",
 ];
+
 let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
+const sounds = {
+    flip: new Audio('./src/sounds/flipcard.mp3'),
+    correct: new Audio('./src/sounds/correct-card.mp3'),
+    win: new Audio('./src/sounds/complete.mp3'),
+}
 
 for(let i = 0; i < emojis.length; i++){
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
-    box.onclick = handleClick
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
 
 function handleClick() {
+    if (this.classList.contains("boxMatch")) {
+        return;
+    }
+
+    if(this.classList.contains("boxOpen")) {
+        return;
+    }
+    
     if(openCards.length < 2) {
         this.classList.add("boxOpen");
         openCards.push(this);
     }
 
     if(openCards.length == 2) {
-        setTimeout(checkMatch, 500);
+        setTimeout(checkMatch, 700);
     }
 }
 
 function checkMatch() {
     if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        sounds.correct.play();
         openCards[0].classList.add("boxMatch");
         openCards[1].classList.add("boxMatch");
     }else {
+        sounds.flip.play();
         openCards[0].classList.remove("boxOpen");
         openCards[1].classList.remove("boxOpen");
     }
@@ -52,6 +68,7 @@ function checkMatch() {
     openCards = [];
 
     if(document.querySelectorAll(".boxMatch").length === emojis.length) {
-        alert("Você venceu!");
+        sounds.win.play();
+        alert("Parabéns! Você encontrou todas as cartas!");
     }
 }
